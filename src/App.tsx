7077d14c3a0b33204724ad2aef5fdd11f90c0674
ValterSMachadojr <machatec41@gmail.com>
2022-02-10 16:43:40 -0300

    ''

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState}from 'react';
 import './App.css';
 import {View1} from 'components/view1';
 import {View2} from 'components/view2';
+import { NodeAPI } from 'services/Service';
+import { get } from 'mongoose';
+import { AxiosResponse } from 'axios';
+import { WelcomeMessageDTO } from 'classes/WelcomeMessageDTO';
 
 function App() {
-  return (
-    <div style={{backgroundColor: 'yellow'}}>
-      <View1 displayText="Texto qualquer">
-       <View2/>
-       <View2/>
-       <View2/>
-       <View2/>
-       
-       <div>texto</div>
-       <View2/>
-      </View1>
-      <View2/>
-      
-    </div>
-  );
-}
+   const [message, setMessage] = useState<string>('');
 
+   async function getWelcomeMessage() {
+    const welcomeMessage:AxiosResponse<WelcomeMessageDTO> = await NodeAPI.get (
+     `${process.env.REACT_APP_API_URL}`
+    );
+    console.log (welcomeMessage.data.mensagem);
+   }
+
+   useEffect(() => {
+       console.log ('Renderizei meu computador');
+     getWelcomeMessage();
+
+  }, []);
+
+ return <> </>;
+  
+}
+  
+  
+  
 export default App;
